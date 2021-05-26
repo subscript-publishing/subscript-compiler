183a81d75fa1a0ef705b3a00741a51b45260b283
Colbyn Wadman <hello@colbyn.com>
2021-05-25 20:35:53 -0600

    whoops

--- src/backend.rs
+++ src/backend.rs
@@ -3,11 +3,11 @@ pub mod ast;
 use ast::*;
 
 pub fn run() {
-    let source = include_str!("../source.txt");
-    let result = crate::parser::run_parser(source)
-        .into_iter()
-        .map(crate::parser::ast::Ast::to_backend)
-        .collect::<Vec<_>>();
-    println!("{:#?}", result);
+    // let source = include_str!("../source.txt");
+    // let result = crate::parser::run_parser(source)
+    //     .into_iter()
+    //     .map(crate::parser::ast::Ast::to_backend)
+    //     .collect::<Vec<_>>();
+    // println!("{:#?}", result);
 }
 
