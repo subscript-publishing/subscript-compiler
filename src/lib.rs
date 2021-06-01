672c9d80bf02a7f02fb823ec6396a9afba76d9a5
Colbyn Wadman <hello@colbyn.com>
2021-05-31 20:07:32 -0600

    add lib

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+#![feature(iter_intersperse)]
+#![feature(linked_list_cursors)]
+#![feature(slice_group_by)]
+#![allow(unused)]
+pub mod codegen;
+pub mod backend;
+pub mod cli;
+pub mod compiler;
+pub mod frontend;
